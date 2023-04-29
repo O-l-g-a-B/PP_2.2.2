5627d64777cb0ceb4772e20caaaf9b2e1a2803d0
O-l-g-a-B <bozhedomova_o@mail.ru>
2023-04-30 00:07:14 +0300

    interface CarService is well

--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -2,13 +2,9 @@ package web.service;
 
 import web.model.Car;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public interface CarService {
-    List<Car> cars = new ArrayList<>();
-    public default List<Car> getNumberOfCars(int count) {
-        return cars;
-    }
+    List<Car> getNumberOfCars(int count);
 }
 
